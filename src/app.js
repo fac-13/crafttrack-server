c05fdd32ae2d801e57e60856ef31b7a9d0036fc8
Lawrence Sarpong <lawrenceeks@yahoo.com>
2018-06-09 14:22:42 +0100

    set up eslint

--- src/app.js
+++ src/app.js
@@ -9,15 +9,15 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(function (_, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:1234");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+	res.header("Access-Control-Allow-Origin", "http://localhost:1234");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
 });
 
 // HELLO ENDPOINT
 
-app.get('/', (req, res) => {
-  res.json({ message: "hello world"});
+app.get("/", (req, res) => {
+	res.json({ message: "hello world"});
 });
 
 // DATABASE ENDPOINTS
@@ -26,9 +26,9 @@ app.get('/', (req, res) => {
 const AWS = require("aws-sdk");
 // Set the region
 AWS.config.update({
-  region: "eu-west-2",
-  accessKeyId: process.env.ACCESS_KEY_ID,
-  secretAccessKey: process.env.SECRET_ACCESS_KEY
+	region: "eu-west-2",
+	accessKeyId: process.env.ACCESS_KEY_ID,
+	secretAccessKey: process.env.SECRET_ACCESS_KEY
 });
 
 // Create the DynamoDB service object
@@ -36,27 +36,27 @@ const ddb = new AWS.DynamoDB({ apiVersion: "2012-10-08" });
 
 // getItems route
 app.get("/getItems", (req, res, next) => {
-  const params = {
-    TableName: "Crafts"
-  };
-  ddb.scan(params, (err, data) => {
-    if (err) {
-      console.log("Error", err);
-      res.json({
-        error: "database error"
-      });
-    } else {
-      res.json({
-        responseData: data
-      });
-      console.log("Success", data);
-    }
-  });
+	const params = {
+		TableName: "Crafts"
+	};
+	ddb.scan(params, (err, data) => {
+		if (err) {
+			console.log("Error", err);
+			res.json({
+				error: "database error"
+			});
+		} else {
+			res.json({
+				responseData: data
+			});
+			console.log("Success", data);
+		}
+	});
 });
 
 // // add new item to make
 app.post("/postItem", (req, res, next) => {
-  ddb.putItem(req.body, (err, data) => err ? res.status(500).json({ error: err.message }) : console.log("Successfully added data"));
+	ddb.putItem(req.body, (err, data) => err ? res.status(500).json({ error: err.message }) : console.log("Successfully added data"));
 });
 
 // // update item status
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-const app = require("./app")
+const app = require("./app");
 const host = process.env.HOST || "localhost";
 const port = process.env.PORT || 3000;
 
 
 app.listen(port, () => {
-  console.log(`app is running on http://${host}:${port}`);
+	console.log(`app is running on http://${host}:${port}`);
 });
