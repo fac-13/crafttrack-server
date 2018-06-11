de51f79a9a7ca8458f2684666f9aec48b31bb6ab
Helen Zhou <helen.zhou6@gmail.com>
2018-06-11 17:21:32 +0100

    Add Access-Control-Allow-Methods - DELETE

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(function (_, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
 	next();
 });
 
